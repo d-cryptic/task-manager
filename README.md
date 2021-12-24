8f060602c6b35f89cb483f8bf09d917f5325cdea
Barun Debnath <barundebnath91@gmail.com>
2021-12-25 00:34:48 +0530

    env file added, mongo atlas connection done

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Task Manager
+
+## Learnings
+
+1. How to setup and connect to cloud database - MongoDB
+2. How to perform CRUD operations
+
+## Project Setup
+
+1. In order to run the project, setup `.env` and set `MONGO_URI` equal to DB connection string.
+2. In the code, port value is `5000`
+3. Run `npm start`
+
+## Packages:
+
+1. `express` - `npm i express`
+2. `nodemon` - `npm i nodemon --D`
+3. `mongoose` - `npm i mongoose`
+4. `dotenv` - `npm i dotenv`
+
+## Rest Api - Representational State Transfer
+
+1. `GET` - api/tasks - Get all tasks
+2. `POST` - api/tasks - Create Task
+3. `GET` - api/tasks/:id - Get Task
+4. `PUT/PATCH` - api/tasks/:id - Update Task
+5. `DELETE` - api/tasks/:id - Delete Task
+
+## MongoDB
+
+- NoSQL, Non Relational DB
+- Store JSON
+- Easy to get started
+- Free Cloud Hosting - Atlas
---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get('/api/v1/tasks')  - get all tasks
+// app.post('/api/v1/tasks') - create a new task
+// app.get('/api/v1/tasks/:id') - get a single task
+// app.patch('/api/v1/tasks/:id') - update a task
+// app.delete('/api/v1/tasks/:id') - delete a task
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
