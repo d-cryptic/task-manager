a8f5f831afb4d4c71ee1de5c676f519e91e5c1fd
Barun Debnath <barundebnath91@gmail.com>
2021-12-28 00:08:50 +0530

    port value added

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(errorHandlerMiddleware);
 // app.patch('/api/v1/tasks/:id') - update a task
 // app.delete('/api/v1/tasks/:id') - delete a task
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
