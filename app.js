94243bf3dd9cc6933bb521b63ca47709b32f497a
Barun Debnath <barundebnath91@gmail.com>
2021-12-27 21:05:21 +0530

    controllers done

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("Task Manager App");
+// });
 
 app.use("/api/v1/tasks", tasks);
 
